d7f245a0da48553788732dc18265f4cbb133d8c2
Kevin Serrano <kevinserrano010@gmail.com>
2025-01-14 19:07:41 -0300

    Creacion de componentes Home and NotFound

---
+++ src/Router.jsx
@@ -0,0 +1,71 @@
+import { createBrowserRouter } from "react-router-dom";
+import { Home } from "./components/Home";
+import { Layout } from "./Layout";
+import { NotFound } from "./components/NotFound";
+
+export const Router = createBrowserRouter([
+    {
+        element: <Layout />,
+        children: [
+            {
+                path: "/login",
+                // element: <Login />
+            },
+            {
+                path: "/",
+                element: <Home />
+            },
+            {
+                path: "/services",
+                children: [
+                    {
+                        index: true,
+                        // element: <Services />
+                    },
+                    {
+                        path: ":idService",
+                        // element: <ServicesDetails />
+                    },
+                    {
+                        path: "addService",
+                        // element: <AddServices />
+                    }
+                ],
+            },
+            {
+                path: "/posts",
+                children: [
+                    {
+                        index: true,
+                        // element: <Posts />
+                    },
+                    {
+                        path: ":idPost",
+                        // element: <PostsDetails />
+                    }
+                ],
+            },
+            {
+                path: "/register",
+                // element: <Register />
+            },
+            {
+                path: "*",
+                element: <NotFound />
+            },
+            {
+                path: "/profile",
+                children: [
+                    {
+                        index: true,
+                        // element: <UserProfile />
+                    },
+                    {
+                        path: "edit",
+                        // element: <UserEdit />
+                    }
+                ]
+            }
+        ]
+    }
+]);
