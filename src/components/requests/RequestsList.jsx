99171be9ef6728be5b551411445cf179f1d3f024
Cristian Villa <vcristian528@gmail.com>
2025-01-17 14:35:09 -0300

    +primera implementacion de publicaciones/posts +primera implementacion de requests

--- src/components/requests/RequestsList.jsx
+++ src/components/requests/RequestsList.jsx
@@ -1,8 +1,43 @@
 import useFetch from '../../hooks/fetchHook';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { useAuth } from "../../contexts/AuthContext";
+import { RequestCard } from './RequestCard';
 
 export const RequestsList = () => {
-    // implementar fetch a la api de solicitudes
+    const { token } = useAuth('state');
+
+    const [{ data: requestsData, isLoading: isLoadingRequests, errors: errorsRequests }, doFetchRequests] = useFetch(`${import.meta.env.VITE_BASE_URL}api/requests/`, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json', 'Authorization': `Token ${token}` },
+    });
+
+    const [{ data: profileData, isLoading: isLoadingProfile, errors: errorsProfile }, doFetchProfile] = useFetch(`${import.meta.env.VITE_BASE_URL}api/profile/`, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json', 'Authorization': `Token ${token}` },
+    });
+
+    const [filteredRequests, setFilteredRequests] = useState([]);
+
+    useEffect(() => {
+        doFetchProfile();
+        doFetchRequests();
+    }, []);
+    
+    useEffect(() => {
+        if (profileData && requestsData) {
+            if(profileData.is_finder){
+            const filtered = requestsData.filter(request => request.user_id === profileData.id);
+            setFilteredRequests(filtered);
+        } else if (profileData.is_supplier){
+            const filtered = requestsData.filter(request => request.post.user.id === profileData.id);
+            setFilteredRequests(filtered);
+        }}
+
+    }, [profileData, requestsData]);
+
+    if (isLoadingRequests || isLoadingProfile) return <div className='container text-center'>Cargando...</div>;
+    if (errorsRequests || errorsProfile) return <div className='container text-center'>Error al cargar los datos.</div>;
+    if (!requestsData || !profileData) return <div className='container text-center'>La Sesión ha expirado, vuelva a iniciar sesión.</div>;
 
     return (
         <div className="container">
@@ -19,10 +54,24 @@ export const RequestsList = () => {
                     <button className="btn btn-outline-dark">Aceptadas</button>
                 </div>
             </div>
-
-            <div className="row">
-                <div className="alert alert-warning text-center" role='alert'>No hay solicitudes disponibles.</div>
-            </div>
+            {/* esto esta horrible pero basicamente si el perfil logueado 
+            es oferente muestra las solicitudes con los botones aceptar y rechazar 
+            si es buscador solo muestra las solicitudes que mando
+            */}
+            <div className="row"> {profileData.is_supplier ? ( 
+                filteredRequests.length === 0 ?
+                ( <div className="alert alert-warning text-center" role='alert'>No hay servicios disponibles para oferentes.</div> 
+                ) : ( filteredRequests.map((request, index) => ( 
+                <div key={index} className="col-12 col-md-4 mb-4"> 
+                <RequestCard key={index} request={request} /> 
+                <div className="btn-group mt-2"> 
+                <button className="btn btn-success">Aceptar</button> 
+                <button className="btn btn-danger">Rechazar</button> </div> </div> )) )
+             ) : ( filteredRequests.length === 0 ? ( 
+             <div className="alert alert-warning text-center" role='alert'>No hay servicios disponibles para buscador.</div>
+              ) : ( filteredRequests.map((request, index) => ( 
+              <div key={index} className="col-12 col-md-4 mb-4">
+                 <RequestCard key={index} request={request} /> </div> )) ) )} </div>
         </div>
-    )
+    );
 };
