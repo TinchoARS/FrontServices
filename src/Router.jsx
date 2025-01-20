d956c8167fe151a290916b319e0470a6b3201ff6
Cjose98 <colquejose53@gmail.com>
2025-01-20 17:53:09 -0300

    +conflicto resuelto

--- src/Router.jsx
+++ src/Router.jsx
@@ -11,6 +11,7 @@ import { PostsList } from "./components/posts/PostsList";
 import { Profile } from "./components/user/Profile";
 import { ProtectedRoute } from "./security/ProtectedRouter";
 import { Ratings } from "./components/Ratings/ratings";
+import { FormServiceAdd } from "./components/services/FormServiceAdd";
 
 export const Router = createBrowserRouter([
     {
