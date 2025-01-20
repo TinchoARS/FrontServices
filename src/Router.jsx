d77eeb1c23892928a348e26844560216b3b8ad68
Kevin Serrano <kevinserrano010@gmail.com>
2025-01-20 16:45:25 -0300

    Creacion de Formulario para crear Publicacion

--- src/Router.jsx
+++ src/Router.jsx
@@ -11,6 +11,7 @@ import { PostsList } from "./components/posts/PostsList";
 import { Profile } from "./components/user/Profile";
 import { ProtectedRoute } from "./security/ProtectedRouter";
 import { FormServiceAdd } from "./components/services/FormServiceAdd";
+import { FormPostAdd } from "./components/posts/FormPostAdd";
 
 export const Router = createBrowserRouter([
     {
@@ -84,6 +85,10 @@ export const Router = createBrowserRouter([
                     {
                         path: ":idPost",
                         // element: <PostsDetails />
+                    },
+                    {
+                        path: "addPost",
+                        element: <FormPostAdd />
                     }
                 ],
             },
