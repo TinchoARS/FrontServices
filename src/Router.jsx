db4bbb6abce1b1136ca54b006b0b5574259d084b
Cjose98 <colquejose53@gmail.com>
2025-01-20 19:16:45 -0300

    +ratings

--- src/Router.jsx
+++ src/Router.jsx
@@ -10,9 +10,9 @@ import { RequestsList } from "./components/requests/RequestsList";
 import { PostsList } from "./components/posts/PostsList";
 import { Profile } from "./components/user/Profile";
 import { ProtectedRoute } from "./security/ProtectedRouter";
+import {Ratings} from "./components/ratings/Ratings";
 import { FormServiceAdd } from "./components/services/FormServiceAdd";
 import { FormPostAdd } from "./components/posts/FormPostAdd";
-import {Ratings} from "./components/ratings/Ratings";
 
 export const Router = createBrowserRouter([
     {
@@ -93,7 +93,7 @@ export const Router = createBrowserRouter([
                     }
                 ],
             },
-            {
+            {   /* ratings */
                 path: "/ratings",
                 children: [
                     {
