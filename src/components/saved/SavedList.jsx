7866d64f71c5fc71b2db27876e369219316ecb5f
Kevin Serrano <kevinserrano010@gmail.com>
2025-01-28 15:59:34 -0300

    Implementacion para guardado de publicaciones.

--- src/components/saved/SavedList.jsx
+++ src/components/saved/SavedList.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../contexts/AuthContext';
@@ -6,64 +7,22 @@ import { useEffect } from 'react';
 import { PostCard } from '../posts/PostCard';
 
 export const SavedList = () => {
-    // const navigate = useNavigate();
-    // const { token } = useAuth('state');
-    // const [ {data, isLoading, errors}, doFetch ] = useFetch(`${import.meta.env.VITE_BASE_URL}api/saved`, {
-    //     method: 'GET',
-    // });
+    const navigate = useNavigate();
+    const { token } = useAuth('state');
+    const [ {data, isLoading, errors}, doFetch ] = useFetch(`${import.meta.env.VITE_BASE_URL}api/savedPosts`, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Token ${token}`
+        }
+    });
 
-    // useEffect(() => {
-    //     doFetch();
-    // }, []);
+    useEffect(() => {
+        doFetch();
+    }, []); // Dependencia vacía para ejecutar solo una vez
 
-    // if (isLoading) return <div className='container text-center'>Cargando...</div>;
-    // if (errors) return <div className='container text-center'>Error al cargar datos.</div>;
-    // if (!data) return <div className='container text-center'>La Sesion ha expirado, vuelva a iniciar sesion.</div>;
-
-    // const data = [
-    //     {
-    //         id: 1,
-    //         id_post: 3,
-    //         id_user: 12,
-    //         post: {
-    //             id: 3,
-    //             description: 'Reparación de computadoras de escritorio y portátiles. Instalación de software y mantenimiento preventivo.',
-    //             category: 'Tecnología',
-    //             user: {
-    //                 id: 12,
-    //                 first_name: 'Juan',
-    //                 last_name: 'Pérez',
-    //                 email: 'juan@gmail.com',
-    //                 telephone: '1234567890',
-    //             },
-    //             service: {
-    //                 id: 1,
-    //                 title: 'Reparación de computadoras',
-    //             }
-    //         }
-    //     },
-    //     {
-    //         id: 2,
-    //         id_post: 4,
-    //         id_user: 13,
-    //         post: {
-    //             id: 4,
-    //             description: 'Reparación de celulares de todas las marcas y modelos. Cambio de pantalla, batería y otros componentes.',
-    //             category: 'Tecnología',
-    //             user: {
-    //                 id: 13,
-    //                 first_name: 'María',
-    //                 last_name: 'Gómez',
-    //                 email: 'maria@gmail.com',
-    //                 telephone: '0987654321',
-    //             },
-    //             service: {
-    //                 id: 2,
-    //                 title: 'Reparación de celulares',
-    //             }
-    //         }
-    //     },
-    // ]
+    if (isLoading) return <div className='container text-center'>Cargando...</div>;
+    if (errors) return <div className='container text-center'>Error al cargar datos.</div>;
+    if (!data) return <div className='container text-center'>La Sesion ha expirado, vuelva a iniciar sesion.</div>;
 
     return (
         <div className="container">
@@ -74,16 +33,16 @@ export const SavedList = () => {
                 </div>
             </div>
             <div className="row">
-            {/* { data.length === 0 ? (
+            { data.length === 0 ? (
                         <div className="alert alert-warning text-center" role='alert'>No hay publicaciones guardadas.</div>
                     ) : (
-                        data.map((post, index) => (
-                            <div key={index} className="col-12 col-md-4 mb-4">
-                                <PostCard key={index} post={post.post} />
+                        data.map((savedPost, index) => (
+                            <div key={index} className="col-12 mb-4">
+                                <PostCard key={index} post={savedPost.post} />
                             </div>
                         ))
                     )
-                } */}
+                }
             </div>
         </div>
     );
