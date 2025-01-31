45439bbdd348f436756e5ae7c3b6aeed409667e5
TinchoFrade <106359821+TinchoARS@users.noreply.github.com>
2025-01-31 00:29:05 -0300

    Revert "agregar token en agregar servicios"

--- src/components/services/FormServiceAdd.jsx
+++ src/components/services/FormServiceAdd.jsx
@@ -3,12 +3,10 @@ import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import useFetch from "../../hooks/fetchHook";
 import { toast } from "react-toastify";
-import { useAuth } from "../../contexts/AuthContext";
 
 
 export const FormServiceAdd = () => {
     const navigate = useNavigate();
-    const { token } = useAuth('state');
     const [formData, setFormData] = useState({
         title: "",
         description: "",
@@ -36,10 +34,6 @@ export const FormServiceAdd = () => {
         `${import.meta.env.VITE_BASE_URL}api/services/`,
         {
         method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Token ${token}`,
-        },
         }
     );
 
